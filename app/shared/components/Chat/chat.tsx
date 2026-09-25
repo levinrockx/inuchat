d8f313c4d26f9ed0d44d557bcc58ea2e877f21b5
Levin E P <levinrockx@gmail.com>
2026-09-25 14:31:22 +0530

    Refactor Chat component to use CSS module for styling and enhance layout structure

--- app/shared/components/Chat/chat.tsx
+++ app/shared/components/Chat/chat.tsx
@@ -3,33 +3,22 @@
 import FlexContainer from "../FlexContainer/FlexContainer";
 import { ButtonVariant } from "../Button/domain/types";
 import { HeaderType } from "../Header/domain/types";
+import classes from './styles/chat.module.css';
 import { IoMdSend } from "react-icons/io";
+import Inu from '../../../assets/inu.png';
 import Header from "../Header/Header";
 import Button from "../Button/Button";
 import Input from "../Input/Input";
-import Inu from '../../../assets/inu.png';
 import Image from "next/image";
 
 const Chat = () => {
-    const sendMessage = async (message: string) => {
-        // const response = await fetch("/api", {
-        //     method: "POST",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     },
-        //     body: JSON.stringify({ message }),
-        // });
-
-        // console.log(response);
-    };
-
     return (
-        <FlexContainer direction="column" className="min-h-[100vh] p-4">
+        <FlexContainer direction="column" className="min-h-[100vh] p-4" gap={10}>
             <FlexContainer alignItem="center" gap={10}>
                 <Image src={Inu} alt="Logo image" height={30} width={30} />
                 <Header type={HeaderType.H1}>InuChat</Header>
             </FlexContainer>
-            <FlexContainer direction="column" className="flex-1">
+            <FlexContainer direction="column" className={classes.chat}>
                 <div></div>
             </FlexContainer>
             <FlexContainer alignItem="stretch" justifyContent="space-between" className="w-full" gap={10}>
